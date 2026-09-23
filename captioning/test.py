03a281e47fc9c5a376d9d8f8faf801249d52417b
Rishabh Saraf <30569562+rishabh2301@users.noreply.github.com>
2026-09-23 11:20:05 +0400

    Add S2VT inference script

---
+++ captioning/test.py
@@ -0,0 +1,30 @@
+
+from module import *
+from utils import *
+
+vovab_size = len(word2id)
+BATCH_SIZE = 10
+
+if __name__ == "__main__":
+    s2vt = S2VT(vocab_size=vovab_size, batch_size=BATCH_SIZE)
+    s2vt = s2vt.cuda()
+    s2vt.load_state_dict(torch.load("/home/r/rishabhs/tasty_dataset/s2vt_pytorch/s2vt_epoch20_iteration100.pkl"))
+    s2vt.eval()
+    for i in range(10):
+        video, caption, cap_mask = fetch_val_data(batch_size=BATCH_SIZE)
+        video = torch.FloatTensor(video).cuda()
+
+        cap_out = s2vt(video)
+
+        captions = []
+        for tensor in cap_out:
+            captions.append(tensor.tolist())
+        # size of captions : [79, batch_size]
+
+        # transform captions to [batch_size, 79]
+        captions = [[row[i] for row in captions] for i in range(len(captions[0]))]
+
+        print('............................\nGT Caption:\n')
+        print_in_english(captions)
+        print('............................\nLABEL Caption:\n')
+        print_in_english(caption)
