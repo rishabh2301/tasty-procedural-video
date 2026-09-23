9458de548698a531ddac2122427eeeabf647e049
Rishabh Saraf <30569562+rishabh2301@users.noreply.github.com>
2026-09-23 11:18:31 +0400

    Add video transforms helper (from pytorch-i3d)

---
+++ features/videotransforms.py
@@ -0,0 +1,35 @@
+import numpy as np
+import numbers
+import random
+
+class CenterCrop(object):
+    """Crops the given seq Images at the center.
+    Args:
+        size (sequence or int): Desired output size of the crop. If size is an
+            int instead of sequence like (h, w), a square crop (size, size) is
+            made.
+    """
+
+    def __init__(self, size):
+        if isinstance(size, numbers.Number):
+            self.size = (int(size), int(size))
+        else:
+            self.size = size
+
+    def __call__(self, imgs):
+        """
+        Args:
+            img (PIL Image): Image to be cropped.
+        Returns:
+            PIL Image: Cropped image.
+        """
+        t, h, w, c = imgs.shape
+        th, tw = self.size
+        i = int(np.round((h - th) / 2.))
+        j = int(np.round((w - tw) / 2.))
+
+        return imgs[:, i:i+th, j:j+tw, :]
+
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(size={0})'.format(self.size)
